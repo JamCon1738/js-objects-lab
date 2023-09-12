255a399b1d610ce2e48ebe47761325a46b37d6fe
Jamie Conlon <20085217@wit.ie>
2023-09-12 11:31:53 +0100

    Loop Constructs.

--- fundamentals.js
+++ fundamentals.js
@@ -2,6 +2,8 @@ const aCar = {
     owner : "Joe Bloggs",
     address: '3 Walkers Lane',
 
+    features : ['Parking assist', 'Alarm', 'Tow-bar'],
+
     previous_owners: [
         { name : ' Pat Smith', address : ' 1 Main Street'},
         { name : ' Sheila Dwyer', address : '2 High Street'}
@@ -26,11 +28,26 @@ const aCar = {
     interiorColour: "cream"
   }
 
-  console.log('First owner : ' + aCar.previous_owners[0].name + address )
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+}
+
+for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+    console.log(aCar.previous_owners[i]) ;
+}
+
+
+for (let p in aCar.type)  {
+    console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+}
+
+
+
+
+
 
 console.log('It is a ' + aCar.colour.exterior + ' car with ' + aCar.mileage + ' mileage, with a ' + aCar.colour.interiorFab + ' interior');
-  
-  
-  console.log(aCar.owner);
 
-  console.log(aCar.owner + ' drives a ' + aCar.type.make)
+console.log(aCar.owner);
+
+console.log(aCar.owner + ' drives a ' + aCar.type.make)
